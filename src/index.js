9a3e1395425f6d8383be904f449253d328d1dd0c
Jasbir Rajrana <68157847+jasbirrajrana@users.noreply.github.com>
2021-04-26 17:45:49 +0530

    init

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import qoutesArray from "../data/qoutes.js";
+
+const getRandomInt = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+let qoute = {};
+const getQuote = () => {
+  let idx = getRandomInt(0, qoutesArray.length - 1);
+  qoute.text = qoutesArray[idx].text;
+  qoute.author = qoutesArray[idx].from;
+  return qoute;
+};
+
+export { getQuote };
